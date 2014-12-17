dcdaeedfc967cd112c8df9851eb133865c878811
Boris <bsadkhin@gmail.com>
2014-12-16 19:26:35 -0600

    Working on view.db.php

--- pages/view.php
+++ pages/view.php
@@ -8,7 +8,7 @@ foreach($rows as $row){
 
     $html_rows .= generateRow($row);
 }
-echo "<table border='1' class='table'>$html_rows</table>";
+echo "<table border='1' >$html_rows</table>";
 
 echo "<submit></submit></form>";
 
@@ -16,42 +16,19 @@ function generateRow($row){
 
     $tds = "";
     $header ="";
-    $tds = "";
-    $header ="";
-    /* foreach($row as $key){
-         switch ($row) {
-             case "venue_id_2":
-                 echo "i equals 0";
-                 break;
-             case 1:
-                 echo "i equals 1";
-                 break;
-             case 2:
-                 echo "i equals 2";
-                 break;
-             default:
-                 $tds .= "<td>$key</td>";
-                 break;
 
-         }
-     }
-         */
 
     foreach(array_keys($row) as $key){
         $value = $row[$key];
         $tds .= "<td>$key<input value='$value'></td>";
 
     }
-    #for($i =0; $i < count($row); $i++){
-    #    if(isset($row[$i])){
-    #    };
-    #}
 
     $colspan = count(array_keys($row));
     return "
             <tr>$header</tr>
             <tr>$tds</tr>
-            ";
+            <tr><td colspan='{$colspan}'></td></tr>";
 
 
 }
