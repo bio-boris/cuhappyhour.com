6a858e6495658debd79e2a021f5698f60bb37a9f
Boris <bsadkhin@gmail.com>
2014-12-16 18:53:46 -0600

    Functionality to grab events , event IDS, and to figure out the location name based on ID

--- pages/view.php
+++ pages/view.php
@@ -1,11 +1,43 @@
 <?php
 
-    $venues = db::getAllVenues();
-    foreach($venues as $key=>$val){
-        print $key;
-        print_r($val);
-        print "<br>";
+    echo "<form>";
+    $rows = db::getAllVenues();
+    $html_rows = array();
+
+    foreach($rows as $row){
+        $html_rows[] = generateRow($row);
     }
+    echo "<table>$html_rows</table>";
+
+    echo "</form>";
+
+   function generateRow($row){
+
+       $tds = "";
+       foreach($row as $key=>$val){
+
+       switch ($row) {
+           case 0:
+               echo "i equals 0";
+               break;
+           case 1:
+               echo "i equals 1";
+               break;
+           case 2:
+               echo "i equals 2";
+               break;
+           default:
+               $tds .= "<td>$key and $val</td>";
+
+       }
+       }
+       return "<tr>$tds</tr>";
+
+
+   }
+
+
+
 
 
 ?>
