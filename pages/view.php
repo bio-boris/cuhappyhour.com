da05b5d0c3862a1f205ebb11bb976c476ac89737
Boris <bsadkhin@gmail.com>
2014-12-16 19:29:01 -0600

    Working on view.db.php

--- pages/view.php
+++ pages/view.php
@@ -12,24 +12,27 @@ echo "<table border='1' >$html_rows</table>";
 
 echo "<submit></submit></form>";
 
-function generateRow($row){
+function generateRow($row)
+{
+
 
     $tds = "";
-    $header ="";
+    $header = "";
 
 
-    foreach(array_keys($row) as $key){
+    foreach (array_keys($row) as $key) {
         $value = $row[$key];
         $tds .= "<td>$key<i value='$value'nput></td>";
 
 
-    $colspan = count(array_keys($row)); #}
+        $colspan = count(array_keys($row)); #}
 
-    return "<tr>$header<
+        return "<tr>$header<
             /tr><tr>$tds<
             <tr><td colspan='{$colspan}'></td></tr>/tr>";
 
 
+    }
 }
 
 
