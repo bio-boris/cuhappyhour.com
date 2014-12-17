4291f959d641ba83bc568c70392ce00a766fb0e8
Boris <bsadkhin@gmail.com>
2014-12-16 19:09:28 -0600

    Functionality to grab events , event IDS, and to figure out the location name based on ID

--- pages/view.php
+++ pages/view.php
@@ -1,42 +1,43 @@
 <?php
 
-    echo "<form>";
-    $rows = db::getAllVenues();
-    $html_rows = array();
+echo "<form>";
+$rows = db::getAllVenues();
+$html_rows = "";
 
-    foreach($rows as $row){
+foreach($rows as $row){
 
-        $html_rows .= generateRow($row);
-    }
-    echo "<table border='1'>$html_rows</table>";
+    $html_rows .= generateRow($row);
+}
+echo "<table border='1'>$html_rows</table>";
 
-    echo "<submit></submit></form>";
+echo "<submit></submit></form>";
 
-   function generateRow($row){
+function generateRow($row){
 
-       $tds = "";
-       $header ="";
-       foreach($row as $key=>$val){
+    $tds = "";
+    $header ="";
+    foreach($row as $key=>$val){
         $header.="<td>$key</td>";
-       switch ($row) {
-           case "venue_id_2":
-               echo "i equals 0";
-               break;
-           case 1:
-               echo "i equals 1";
-               break;
-           case 2:
-               echo "i equals 2";
-               break;
-           default:
-               $tds .= "<td>$val</td>";
+        switch ($row) {
+            case "venue_id_2":
+                echo "i equals 0";
+                break;
+            case 1:
+                echo "i equals 1";
+                break;
+            case 2:
+                echo "i equals 2";
+                break;
+            default:
+                $tds .= "<td>$val</td>";
+                break;
 
-       }
-       }
-       return "<tr>$header</tr><tr>$tds</tr>";
+        }
+    }
+    return "<tr>$header</tr><tr>$tds</tr>";
 
 
-   }
+}
 
 
 
