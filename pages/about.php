22a0b94bd0bf0fd5e0668e2a5ed453d87dfd0ccf
bio-boris <bio-boris@users.noreply.github.com>
2015-03-22 09:27:20 -0500

    Update about.php

--- pages/about.php
+++ pages/about.php
@@ -3,4 +3,5 @@
 	echo "<img src='media/about.jpg'/ width='60%'>";
 	echo "<p><h4>I've never worn sunglasses in my life, so you can trust my Happy Hour Deals, bro.</h4></p>";
 	echo "</div>";
+	echo "JERED IS COOL";
 ?>
